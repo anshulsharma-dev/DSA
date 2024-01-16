3339a5da4830f164cfe3ea3cbf4f8517e7cf3fef
Your Name <sharmaanshul9796@gmail.com>
2024-01-17 03:22:31 +0530

    l

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const about = () => {
   console.log("software engineer");
-}
+};
+
+// Pattern Problem
+const patterProblem = (n) => {
+  // loop to repeat a task for n rows
+  for (let i = 1; i <= n; i++) {
+    let str = "";
+    for (let j = 1; j <= n; j++) {
+      str += "*";
+    }
+    // console.log(str);
+  }
+};
+
+patterProblem(4);
+patterProblem(9);
+patterProblem(3);
